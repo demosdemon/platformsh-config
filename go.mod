a8cf1f042d61b40c6564e16c453131165b836b13
Brandon LeBlanc <brandon@platform.sh>
2019-04-08 18:29:11 -0500

    refactor the hell out of this

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/demosdemon/platformsh-config
 go 1.11
 
 require (
-	github.com/elgs/gojq v0.0.0-20160421194050-81fa9a608a13
-	github.com/elgs/gosplitargs v0.0.0-20161028071935-a491c5eeb3c8 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/bhmj/jsonslice v0.0.0-20190301184140-597c3cda2260
+	github.com/oliveagle/jsonpath v0.0.0-20180606110733-2e52cf6e6852 // indirect
 	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5
 )
--- main.go
+++ main.go
@@ -1,79 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
+	"net/url"
 	"os"
 	"text/template"
 
-	"github.com/elgs/gojq"
+	"github.com/bhmj/jsonslice"
 	"golang.org/x/crypto/bcrypt"
 )
 
-type jsonMap = map[string]interface{}
+type jsonmap = map[string]interface{}
 
-type config struct {
-	TokenURL    string
-	RegistryURL string
-}
+const logFlags = log.LstdFlags | log.Lshortfile | log.LUTC
 
 func main() {
+	log.SetOutput(os.Stderr)
+	log.SetFlags(logFlags)
+
 	configTemplate, err := ioutil.ReadFile("config.yml")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "E unable to read template: %v\n", err)
-		os.Exit(4)
+		log.Fatalf("unable to read template: %v", err)
 	}
-
-	application, _ := getJSONVariable("PLATFORM_APPLICATION")
-	variables, _ := getJSONVariable("PLATFORM_VARIABLES")
-	routes, _ := getJSONVariable("PLATFORM_ROUTES")
-	rels, _ := getJSONVariable("PLATFORM_RELATIONSHIPS")
 
 	funcMap := template.FuncMap{
 		"env":      os.Getenv,
 		"hostname": os.Hostname,
-		"app":      application.Query,
-		"var":      variables.Query,
-		"route":    routes.Query,
-		"rel":      rels.Query,
 		"bcrypt":   hashBcrypt,
+		"app":      getJSONSlice("PLATFORM_APPLICATION"),
+		"var":      getJSONSlice("PLATFORM_VARIABLES"),
+		"route":    getRoutes(),
+		"rel":      getJSONSlice("PLATFORM_RELATIONSHIPS"),
+		"slice": func(data, path string) (string, error) {
+			sub := returnSlice([]byte(data))
+			return sub(path)
+		},
+		"json": func(data string) (interface{}, error) {
+			var v interface{}
+			err = json.Unmarshal([]byte(data), &v)
+			return v, err
+		},
 	}
 
-	tpl := template.New("config.yml")
-	tpl = tpl.Funcs(funcMap)
-	tpl, err = tpl.Parse(string(configTemplate))
+	tpl, err := template.New("config.yml").Funcs(funcMap).Parse(string(configTemplate))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "E unable to compile template: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("unable to compile template: %v", err)
 	}
 
-	cfg := new(config)
-	if routeMap, ok := routes.Data.(jsonMap); ok {
-		for url, v := range routeMap {
-			if vMap, ok := v.(jsonMap); ok {
-				if upstream, ok := vMap["upstream"]; ok {
-					if s, ok := upstream.(string); ok {
-						switch s {
-						case "auth":
-							cfg.TokenURL = url
-						case "registry":
-							cfg.RegistryURL = url
-						}
-					}
-				}
-			} else {
-				fmt.Fprintf(os.Stderr, "failed to convert vMap %s %#v\n", url, v)
-			}
-		}
+	buffer := &bytes.Buffer{}
+	if err := tpl.Execute(buffer, nil); err == nil {
+		io.Copy(os.Stdout, buffer)
 	} else {
-		fmt.Fprintf(os.Stderr, "failed to convert routeMap %#v\n", routes.Data)
-	}
-
-	err = tpl.Execute(os.Stdout, cfg)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "E unable to render template: %v\n", err)
-		os.Exit(2)
+		log.Fatalf("unable to render template: %v", err)
 	}
 }
 
@@ -85,16 +70,69 @@ func hashBcrypt(password string) (string, error) {
 	return string(passwordBytes), nil
 }
 
-func getJSONVariable(key string) (*gojq.JQ, error) {
+func getJSONSlice(key string) func(string) (string, error) {
 	value, ok := os.LookupEnv(key)
 	if !ok {
-		return nil, fmt.Errorf("key %q not found", key)
+		return returnError(fmt.Errorf("environment variable %s not found", key))
 	}
 
 	jsonValue, err := base64.StdEncoding.DecodeString(value)
 	if err != nil {
-		return nil, err
+		return returnError(err)
 	}
 
-	return gojq.NewStringQuery(string(jsonValue))
+	return returnSlice(jsonValue)
+}
+
+func getRoutes() func(string) (string, error) {
+	value, ok := os.LookupEnv("PLATFORM_ROUTES")
+	if !ok {
+		return returnError(errors.New("environment variable PLATFORM_ROUTES not found"))
+	}
+
+	jsonValue, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+		return returnError(err)
+	}
+
+	var routes jsonmap
+	err = json.Unmarshal(jsonValue, &routes)
+	if err != nil {
+		return returnError(err)
+	}
+
+	var result = make([]jsonmap, 0, len(routes))
+	for k, v := range routes {
+		m, ok := v.(jsonmap)
+		if !ok {
+			return returnError(fmt.Errorf("invalid value for %s: %#v", k, v))
+		}
+
+		m["url"], err = url.Parse(k)
+		if err != nil {
+			return returnError(err)
+		}
+
+		result = append(result, m)
+	}
+
+	jsonValue, err = json.Marshal(result)
+	if err != nil {
+		return returnError(err)
+	}
+
+	return returnSlice(jsonValue)
+}
+
+func returnError(err error) func(string) (string, error) {
+	return func(string) (string, error) {
+		return "", err
+	}
+}
+
+func returnSlice(data []byte) func(string) (string, error) {
+	return func(path string) (string, error) {
+		v, err := jsonslice.Get(data, path)
+		return string(v), err
+	}
 }
